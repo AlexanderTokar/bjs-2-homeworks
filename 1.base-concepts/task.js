3440525fde8873730de5fcf041379e6f55857c25
AlexanderTokar <alextokar@inbox.ru>
2022-10-28 22:17:54 +0300

    Task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,21 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  arr = [];
+
+  let discriminant = (b ** 2) - (4 * a * c);
+
+  if (discriminant === 0) {
+      let root1 = -b / (2 * a);
+      arr.push(root1);
+  }
+  if (discriminant > 0) {
+      let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+      let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+      arr.push(root1);
+      arr.push(root2);
+  }
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
