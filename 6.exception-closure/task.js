278dbea305c9d2c754d43e66aa7ba76071721dcc
AlexanderTokar <alextokar@inbox.ru>
2022-11-14 21:04:45 +0300

    Add Tasks 1-2

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,58 @@
+// Задача 1
+
+function parseCount(value) {
+    let element = Number.parseInt(value);
+    if (isNaN(element)) {
+        throw new Error("Невалидное значение");
+    }
+    return element;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (((a + b) <= c) || ((a + c) <= b) || ((b + c) <=a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let semiPerimeter = this.getPerimeter() / 2;
+        return Number.parseFloat(Math.sqrt((semiPerimeter *
+            (semiPerimeter - this.a) *
+            (semiPerimeter - this.b) *
+            (semiPerimeter - this.c))).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    let triangle = {};
+    try {
+        triangle = new Triangle(a, b, c);
+    } catch (error) {
+        triangle.getPerimeter = function() {
+            return "Ошибка! Треугольник не существует"
+        };
+        triangle.getArea = function() {
+            return "Ошибка! Треугольник не существует"
+        }
+    }
+    return triangle;
+}
